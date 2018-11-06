5f4d7b359ccf2ffdb0888afa1706005b2edb4f2c
Unknown <gamiysikdima@gmail.com>
2018-11-06 21:18:36 +0300

    login completed

--- app.py
+++ app.py
@@ -1,33 +1,47 @@
 import db
 import os
-from flask import redirect, render_template, url_for, request, Flask
+from flask import redirect, render_template, url_for, request, Flask, Session
+from flask_login import LoginManager, login_user, login_required, current_user, logout_user
 
 template_dir = os.path.abspath('html')
 template_dir = os.path.join(template_dir, 'templates')
 print(template_dir)
 app = Flask(__name__, template_folder=template_dir)
+app.secret_key = 'secretkey'
+login_manager = LoginManager(app)
+login_manager.login_view = 'auth'
 
 
 @app.route('/index')
+@login_required
 def index():
-    return 'Hello!'
+    return render_template('index.html')
 
 
 @app.route('/auth', methods=['GET', 'POST'])
 def auth():
     error = None
     if request.method == 'POST':
-        print(request.get_data())
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+        db_resp = db.check_admin(request.form['username'], request.form['password'])
+        if db_resp == 0:
             error = 'Invalid Credentials. Please try again.'
         else:
-            return 'Logged in'
+            user = db.get_user(db_resp)
+            login_user(user)
+
+            return redirect(url_for('index'))
     return render_template('login.html', error=error)
 
 
 @app.route('/login', methods=['POST'])
 def hello_guest():
     return str(db.login(request.values.get('card')))
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 
 @app.route('/add', methods=['POST'])
@@ -55,6 +69,11 @@ def delete_card_user():
 @app.route('/delete_name_user', methods=['POST'])
 def delete_name_user():
     return str(db.delete_name_user(request.values.get('name')))
+
+
+@login_manager.user_loader
+def load_user(id):
+    return db.get_user(id)
 
 
 if __name__ == '__main__':
--- db.py
+++ db.py
@@ -2,7 +2,17 @@ import postgresql
 import constants
 import datetime
 import random
+from flask_login import UserMixin
 
+
+class User(UserMixin):
+    id = ''
+
+    def __init__(self, id):
+        self.id = id
+
+    def get_id(self):
+        return self.id
 
 def init():
     constants.DB = postgresql.open('pq://' + constants.username + ':' + constants.password + '@' + constants.DBIP
@@ -41,3 +51,24 @@ def get_all_users():
             need_str += str(j) + '}'
         need_str += '{'
     return need_str
+
+
+def check_admin(admin_login, password):
+    email_resp = constants.DB.query("select id from public.admins where email= '" + admin_login +
+                                    "' and password = crypt('" + password + "', password)")
+    login_resp = constants.DB.query("select id from public.admins where login= '" + admin_login +
+                                    "' and password = crypt('" + password + "', password)")
+    if email_resp.__len__() > 0:
+        return email_resp[0][0]
+    elif login_resp.__len__() > 0:
+        return login_resp[0][0]
+    return 0
+
+
+def get_user(uid):
+    resp = constants.DB.query("select is_authenticated, is_active, is_anonymous from public.admins where id =" + str(uid))
+    user = None
+    if resp.__len__() > 0:
+        user = User(str(uid))
+
+    return user
