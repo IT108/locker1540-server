b00c5a2b308e18274814df69e1f7c31e7eac2722
Unknown <gamiysikdima@gmail.com>
2018-12-23 20:21:29 +0300

    update_script

---
+++ commit_updates.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+su it108_admin
+ITCHURCH
+sudo -s
+ITCHURCH
+S_SEP="'"
+S_DATE=$(date)
+S_PREFIX="SERVER_UPDATE_"
+S_RES=$S_SEP$S_PREFIX$S_DATE$S_SEP
+echo $S_RES
+cd /var/www/locker/server
+git add *
+git commit -m "$S_RES"
+git push
