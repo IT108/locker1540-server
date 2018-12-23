146ed3f745bc740dd67aa6547b9050688724fea8
root <root@176-99-11-114.cloudvps.regruhosting.ru>
2018-12-23 22:36:06 +0300

    SERVER_UPDATE

--- commit_updates.sh
+++ commit_updates.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/expect -f
-S_SEP="'"
-S_DATE=$(date)
-S_PREFIX="SERVER_UPDATE_"
-S_RES=$S_SEP$S_PREFIX$S_DATE$S_SEP
 set timeout -1
 spawn /bin/su root
 expect "Password: "
 send "ITCHURCH\r"
 expect "\r\n"
-send "cd /var/www/locker/server\rgit add *\rgit commit -m "$S_RES"\rgit push"
+send "cd /var/www/locker/server\r"
+expect "root@176-99-11-114:/var/www/locker/server# "
+send "git add *\r"
+expect "root@176-99-11-114:/var/www/locker/server# "
+send "git commit -m 'SERVER_UPDATE'\r"
+expect "root@176-99-11-114:/var/www/locker/server# "
+send "git push\r"
+expect "root@176-99-11-114:/var/www/locker/server# "
+
